af735fbb8894d558547f825461683db498ac4960
Shaun Mulligan <shaun@balena.io>
2020-06-17 14:52:30 +0200

    working with basic inference, no webstream

--- edge-logic/run.sh
+++ edge-logic/run.sh
@@ -3,7 +3,7 @@
 # Run a simple classification on the supplied image. 
 # If the model or labels are not available, the container 
 # will exit and try again.
-python3 src/classify_image.py \
+python3 app/app.py \
 --model /models/model.tflite \
 --label /models/labels.txt \
 --image images/parrot.jpg
